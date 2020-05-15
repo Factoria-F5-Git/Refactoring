5f9b81636a2f6bdd22aa6cf2c07d1e15fb034636
Tsvetan Nenov <tsvetan.nenov@thoughtworks.com>
2020-05-15 17:10:29 +0200

    Add tests and initial implementation for Soldier

--- src/main/java/extractClass.java
+++ src/main/java/extractClass.java
@@ -1,13 +1,29 @@
 class Soldier {
+
+    private static final int WEAPON_READY = 1;
+    private static final int WEAPON_BROKEN = 0;
+
     public int health;
     public int damage;
     public int weaponStatus;
 
-    public void getDamage() {
-        // ...
+    public Soldier(int health, int damage, int weaponStatus) {
+        this.health = health;
+        this.damage = damage;
+        this.weaponStatus = weaponStatus;
     }
 
-    public void attack() {
-        // ...
+    public int getDamage() {
+        if (this.weaponStatus == WEAPON_READY) {
+            return damage;
+        } else {
+            return 0;
+        }
+    }
+
+    public void attack(Soldier soldier) {
+        if (this.weaponStatus == WEAPON_READY) {
+            soldier.health -= damage;
+        }
     }
 }
